c2b7b8e2068740a372d7548aafb478fd09649b31
cendress <95492688+cendress@users.noreply.github.com>
2024-01-15 12:03:31 -0500

    Configure collection view

--- Pairs/ViewController.swift
+++ Pairs/ViewController.swift
@@ -7,12 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+  
+  var capitalCities: [String: String] = ["Paris": "France", "London": "England", "Bangkok": "Thailand", "Beijing": "China", "Mexico City": "Mexico", "Geneva": "Switzerland", "Berlin": "Germany", "Tokyo": "Japan"]
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
+  
+  //MARK: - Collection view methods
+  
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return capitalCities.count / 2
+  }
+  
+  override func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 4
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+    cell.layer.cornerRadius = 20
+    cell.backgroundColor = .systemPurple
+    return cell
+  }
+  
+  //MARK: - UICollectionViewDelegateFlowLayout method
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 128, height: 200)
+  }
+  
+  //create cards
+  //create a collection view containing the cards
+  //create an onTap method that determines whats done once the card is tapped
+  //only allow two cards to be flipped at a time for a new action is done
+  
+  //cards must come in pairs of two (dictionary)
+  //create a dictionary of pairing values that contain the same key or index?
+  
+  //if two cards match remove them and update the score
+  //if both cards don't match then flip them over again
 
 
 }
